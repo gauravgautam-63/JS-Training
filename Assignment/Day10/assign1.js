eff2e4a5a7e84203c2272004583c562779d1e6de
Gaurav Gautam <gautamgaurav2063@gmail.com>
2025-05-18 17:14:34 +0545

    Add files via upload

--- Assignment/Day10/assign1.js
+++ Assignment/Day10/assign1.js
@@ -1,1 +1,13 @@
-let num = +prompt("Enter a number to find factorial:")
+// Factorial Finder
+
+let num = parseInt(prompt("Enter a positive integer to find factorial:"));
+
+if (isNaN(num) || num <= 0) {
+  alert("Invalid input! Please enter a positive integer.");
+} else {
+  let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  alert(" Factorial of " + num + " is " + factorial);
+}
